9e2cf36b7d77ca4349f75f4cf8a7a41e1b816d2e
Eduardo Flores <eduardo@enelmargen.org>
2016-03-23 21:28:49 -0600

    Update SetupDespliegue.R

--- R/SetupDespliegue.R
+++ R/SetupDespliegue.R
@@ -28,7 +28,7 @@ SetupDespliegue <- function(folder_datos, folder_scripts, mailpassword) {
   # ++++++++++++++++++++++++++++++++++++++++++++++++++++++
   # List of packages for session
   .packages = c("ggplot2", "dplyr", "magrittr", "rvest", "stringi", "stringr",
-                "inegiR", "devtools")
+                "inegiR", "devtools", "lubridate", "mailR")
 
   # Install CRAN packages (if not already installed)
   .inst <- .packages %in% installed.packages()
